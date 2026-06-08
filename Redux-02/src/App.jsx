5d53c7d28072f42897daa64ffe79ddbbe2efdc09
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-06-08 11:34:17 +0500

    Redux-02

--- Redux-02/src/App.jsx
+++ Redux-02/src/App.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import { pexelsApi } from './Api/mediaApi.js'
+import React from "react";
+import { giphyApi, pexelsApi } from "./Api/mediaApi.js";
+import { unsplashApi } from "./Api/mediaApi.js";
 const App = () => {
+  return (
+    <>
+      <div className="text-white h-screen bg-gray-900">
+        <button
+          className="bg-red-500 m-5 p-3 hover:bg-red-600 active:bg-red-700"
+          onClick={() => {
+            unsplashApi("cat");
+          }}
+        >
+          GET PHOTOS
+        </button>
+        <button
+          className="bg-red-500 m-5 p-3 hover:bg-red-600 active:bg-red-700"
+          onClick={() => {
+            pexelsApi("cat");
+          }}
+        >
+          GET VIDEOS
+        </button>
 
-  return (<>
-    <div className='text-white h-screen bg-gray-900'>
+          <button
+          className="bg-red-500 m-5 p-3 hover:bg-red-600 active:bg-red-700"
+          onClick={() => {
+            giphyApi('cheeseburger')
+          }}
+        >
+          GET GIPHY
+        </button>
+      </div>
+    </>
+  );
+};
 
-      <button className='bg-red-500' onClick={()=>{ pexelsApi('cat')}}>GET PHOTOS</button>
-    </div>
-  </>
-  )
-}
-
-export default App
+export default App;
--- Redux-02/src/api/mediaApi.js
+++ Redux-02/src/api/mediaApi.js
@@ -2,24 +2,37 @@ import axios from 'axios'
 
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
+const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY
 
-export const unsplashApi = async (query, page = 1) => {
+export const unsplashApi = async (query, page = 1, per_page = 20) => {
     try {
         const res = await axios.get('https://api.unsplash.com/search/photos', {
-            params: { query, page }, headers: { Authorization: `Client-ID ${UNSPLASH_KEY}` }
+            params: { query, page, per_page }, headers: { Authorization: `Client-ID ${UNSPLASH_KEY}` }
         })
-        console.log(res)
+        console.log(res.data.results)
     } catch (err) {
         console.error(err)
     }
 }
 
-export const pexelsApi = async (query, page = 1) => {
+export const pexelsApi = async (query, page = 1, per_page = 15) => {
     try {
         const res = await axios.get('https://api.pexels.com/v1/videos/search', {
             params: { query, page }, headers: { Authorization: PEXELS_KEY }
         })
-        console.log(res)
+        console.log(res.data.videos)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+export const giphyApi = async (query, page = 1) => {
+    try {
+        const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
+            params: { q: query, limit: 20, api_key: GIPHY_KEY }
+        })
+        console.log(res.data.data)
     } catch (err) {
         console.error(err)
     }
